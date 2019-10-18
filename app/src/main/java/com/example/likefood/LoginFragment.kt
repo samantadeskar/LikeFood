2b6a8f20efd4446979bbbe25e136929724c57b0f
Samanta <samanta.deskar@gmail.com>
2019-10-18 14:01:10 +0200

    add login screen

---
+++ app/src/main/java/com/example/likefood/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.example.likefood
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+    companion object {
+        fun newInstance(): LoginFragment{
+            return LoginFragment()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container,false)
+        return view
+    }
+}
--- app/src/main/java/com/example/likefood/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/likefood/LoginRegisterActivity.kt
@@ -3,10 +3,10 @@ package com.example.likefood
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginRegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_register)
     }
 }
